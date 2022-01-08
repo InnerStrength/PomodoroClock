7ae1c61c8a5c50a8c5ac35cb577d8764e235ea5d
InnerStrength <patrickngp@gmail.com>
2022-01-08 03:00:50 -0500

    Update main.py
    PEP fix. Added pop-up functionality when it's time to take your break. Also set window to generate in bottom right of the users screen, so as to be non intrusive.
    
    This should be the final version (minus some minor aesthetic tweaks I may do for my own purposes.

--- main.py
+++ main.py
@@ -1,46 +1,55 @@
 from tkinter import *
 from math import floor
+
 # ---------------------------- CONSTANTS ------------------------------- #
 WORK_MIN = 25 * 60
 SHORT_BREAK_MIN = 5 * 60
 LONG_BREAK_MIN = 20 * 60
 FILL = "#570200"
-MARKS = ["✔","✔ ✔","✔ ✔ ✔","✔ ✔ ✔ ✔"]
+MARKS = ["✔", "✔ ✔", "✔ ✔ ✔", "✔ ✔ ✔ ✔"]
 LABEL = ["Work Work Work", "5 Minute Break", "Take 20"]
 check = 0
 timer = 0
-# ---------------------------- TIMER RESET ------------------------------- # 
+
+
+# ---------------------------- TIMER RESET ------------------------------- #
 def reset_timer():
-    global check, timer, clock, timer_label, reps
+    global check, timer, clock, timer_label
     window.after_cancel(timer)
     check = 0
     canvas.itemconfig(clock, text="00:00")
     timer_label.config(text="Pomodoro Timer", bg="pink")
-    reps = 0
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
     global check
     check += 1
+
+    window.attributes("-topmost", True)
+    window.update()
+    window.attributes("-topmost", False)
 
     if check >= 2:
         mark = floor(int(check / 2) - 1)
         canvas.itemconfig(checkmark, text=MARKS[mark])
 
-    if check% 8 == 0:
+    if check % 8 == 0:
         count_down(LONG_BREAK_MIN)
         timer_label.config(text=LABEL[2], bg="pink")
         check = 0
-    elif check%2 == 0:
+    elif check % 2 == 0:
         count_down(SHORT_BREAK_MIN)
         timer_label.config(text=LABEL[1], bg="yellow")
     else:
         count_down(WORK_MIN)
         timer_label.config(text=LABEL[0], bg="light green")
 
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
     global timer
+
     if count >= 0:
         timer = window.after(1000, count_down, count - 1)
         min = int(count / 60)
@@ -53,32 +62,35 @@ def count_down(count):
     else:
         start_timer()
 
-# ---------------------------- UI SETUP ------------------------------- #
+
+# ---------------------------- Window SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro Timer")
 window.minsize(width=444, height=480)
 window.maxsize(width=444, height=480)
+x = window.winfo_screenwidth()-450
+y = window.winfo_screenheight()-550
+window.geometry('%dx%d+%d+%d' % (444, 480, x, y))
 
 
+# ---------------------------- GUI ---------------------------------------- #
 canvas = Canvas(width=460, height=550)
 bg_image = PhotoImage(file="moon.gif")
 
 canvas.create_image(220, 380, image=bg_image)
 clock = canvas.create_text(222, 162, text="00:00", font=("Courier", 42, "bold"), fill=FILL)
-checkmark = canvas.create_text(222, 250, text=" ", font=("verdana",23,"normal"), fill=FILL)
-canvas.place(x=-2,y=0)
+checkmark = canvas.create_text(222, 250, text=" ", font=("verdana", 23, "normal"), fill=FILL)
+canvas.place(x=-2, y=0)
 
 start_button = Button(text="Start", bg="light blue")
-start_button.place(x=30,y=30,width=60)
+start_button.place(x=30, y=30, width=60)
 start_button.config(command=start_timer)
 timer_label = Label(text="Pomodoro Timer", width=25, height=2, font=("Courier", 18, "normal"), bg="pink")
 timer_label.place(x=40, y=370)
 
 
 reset_button = Button(text="Reset", bg="light blue")
-reset_button.place(x=354,y=30,width=60)
+reset_button.place(x=354, y=30, width=60)
 reset_button.config(command=reset_timer)
 
 window.mainloop()
-
-
